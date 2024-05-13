5a9d44bed21d59c5b101d6ac04f25d8fe5033886
kyphan38 <kyphan.work@gmail.com>
2024-05-13 15:32:31 +0700

    feat: modify data pipeline

--- src/data/pipeline/pipeline/__init__.py
+++ src/data/pipeline/pipeline/__init__.py
@@ -1,7 +1,14 @@
 from dagster import Definitions
+
+# Import assets from each layer
 from .assets.bronze_layer import generate_assets
-from .assets.silver_layer import location_table, listing_table, host_table, review_table, fact_table
+from .assets.silver_layer import location_table as silver_location_table, listing_table as silver_listing_table, host_table as silver_host_table, review_table as silver_review_table, fact_table as silver_fact_table
+from .assets.gold_layer import listing_table as gold_listing_table, host_table as gold_host_table, review_table as gold_review_table, fact_table as gold_fact_table
+from .assets.warehouse_layer import listing_table as wh_listing_table, host_table as wh_host_table, review_table as wh_review_table, fact_table as wh_fact_table
+
+# Import resources
 from .resources.minio_io_manager import MinIOIOManager
+from .resources.psql_io_manager import PostgreSQLIOManager
 
 from dotenv import load_dotenv
 import os
@@ -9,16 +16,36 @@ load_dotenv()
 
 # Setting default values in case environment variables are missing
 MINIO_CONFIG = {
-    "minio_endpoint_url": os.getenv("MINIO_ENDPOINT_URL", "minio:9000"),
-    "minio_access_key_id": os.getenv("AWS_ACCESS_KEY_ID", "minio"),
-    "minio_secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY", "minio123"),
-    "minio_bucket": os.getenv("MINIO_BUCKET", "warehouse")
+  "minio_endpoint_url": os.getenv("MINIO_ENDPOINT_URL", "minio:9000"),
+  "minio_access_key_id": os.getenv("AWS_ACCESS_KEY_ID", "minio"),
+  "minio_secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY", "minio123"),
+  "minio_bucket": os.getenv("MINIO_BUCKET", "warehouse")
 }
+
+FEAST_POSTGRES_CONFIG = {
+  "host": os.getenv("FEAST_POSTGRES_HOST", "feast_postgres"),
+  "port": os.getenv("FEAST_POSTGRES_PORT", "5432"),
+  "database": os.getenv("FEAST_POSTGRES_DB", "feast_postgres"),
+  "user": os.getenv("FEAST_POSTGRES_USER", "admin"),
+  "password": os.getenv("FEAST_POSTGRES_PASSWORD", "admin123"),
+}
+
+# FEAST_POSTGRES_CONFIG = {
+#   "host": os.getenv("DAGSTER_POSTGRES_HOST", "dagster_postgres"),
+#   "port": os.getenv("DAGSTER_POSTGRES_PORT", "5432"),
+#   "database": os.getenv("DAGSTER_POSTGRES_DB", "dagster_postgres"),
+#   "user": os.getenv("DAGSTER_POSTGRES_USER", "admin"),
+#   "password": os.getenv("DAGSTER_POSTGRES_PASSWORD", "admin123"),
+# }
 
 # Initialize definitions with dynamically generated assets
 defs = Definitions(
-  assets=[location_table, listing_table, host_table, review_table, fact_table] + generate_assets("minio_io_manager"),
+  assets=generate_assets("minio_io_manager") +
+  [silver_location_table, silver_listing_table, silver_host_table, silver_review_table, silver_fact_table] +
+  [gold_listing_table, gold_host_table, gold_review_table, gold_fact_table] +
+  [wh_listing_table, wh_host_table, wh_review_table, wh_fact_table],
   resources={
-    "minio_io_manager": MinIOIOManager(MINIO_CONFIG)
+    "minio_io_manager": MinIOIOManager(MINIO_CONFIG),
+    "psql_io_manager": PostgreSQLIOManager(FEAST_POSTGRES_CONFIG)
   }
 )
--- src/data/pipeline/pipeline/resources/psql_io_manager.py
+++ src/data/pipeline/pipeline/resources/psql_io_manager.py
@@ -1,9 +1,27 @@
-import os
+from contextlib import contextmanager
 import pandas as pd
-import glob
+from dagster import IOManager, OutputContext, InputContext
+from sqlalchemy import create_engine
+import pyarrow.parquet as pq
+import os
 from contextlib import contextmanager
 from dagster import IOManager, OutputContext, InputContext
 from minio import Minio, S3Error
+
+
+@contextmanager
+def connect_psql(config):
+  conn_info = (
+    f"postgresql+psycopg2://{config['user']}:{config['password']}"
+    + f"@{config['host']}:{config['port']}"
+    + f"/{config['database']}"
+  )
+
+  db_conn = create_engine(conn_info)
+  try:
+    yield db_conn
+  finally:
+    db_conn.dispose()
 
 @contextmanager
 def connect_minio(config):
@@ -23,37 +41,32 @@ def connect_minio(config):
     print(f"Error connecting to MinIO: {e}")
     raise
 
-class MinIOIOManager(IOManager):
+class PostgreSQLIOManager(IOManager):
   def __init__(self, config):
     self._config = config
 
   def _get_path(self, context):
-    layer = context.asset_key.path[0]
+    layer, table = context.asset_key.path
     key_name = os.path.join(layer, *context.asset_key.path[1:]) + ".parquet"
     tmp_dir_path = f"/tmp/{layer}/"
 
     os.makedirs(tmp_dir_path, exist_ok=True)
     tmp_file_path = os.path.join(tmp_dir_path, os.path.basename(key_name))
 
-    return key_name, tmp_file_path
+    return key_name, tmp_file_path, table
 
-  def handle_output(self, context: OutputContext, obj:pd.DataFrame):
-    key_name, tmp_file_path = self._get_path(context)
-    obj.to_parquet(tmp_file_path, index=False)
+  def handle_output(self, context: OutputContext, obj: pd.DataFrame):
+    key_name, tmp_file_path, table = self._get_path(context)
 
     try:
-      bucket_name = self._config.get("minio_bucket")
-      with connect_minio(self._config) as client:
-        found = client.bucket_exists(bucket_name)
-        if not found:
-          client.make_bucket(bucket_name)
-        else:
-          context.log.info(f"Bucket {bucket_name} already exists")
-        client.fput_object(bucket_name, key_name, tmp_file_path)
-        context.log.info(f"Parquet file uploaded to MinIO at {key_name}")
+      with connect_psql(self._config) as engine:
+        obj.to_sql(table, engine, if_exists="replace", index=False)
+        context.log.info("Write to PostgreSQL successfully!")
     except Exception as e:
-      context.log.error(f"Error during MinIO upload: {e}")
+      context.log.error(f"Error writing to PostgreSQL: {e}")
       raise
+
+    print("Write successfully!")
 
   def load_input(self, context: InputContext):
     key_name, tmp_file_path = self._get_path(context)
